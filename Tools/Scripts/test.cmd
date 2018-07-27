e707e47c360e610ed90f96758cb93052adfc2a29
James John McGuire <jamesjohnmcguire@gmail.com>
2018-07-27 12:44:01 +0000

    Updated notes, packages, test command (still not working right)

--- Tools/Scripts/test.cmd
+++ Tools/Scripts/test.cmd
@@ -1,7 +1,18 @@
-cd "\Users\James Mcguire\Data\Projects\DigitalZenWorks\Backup\FlickrStorage\bin\Debug"
-del /Q "lPg7wmAA64wCTygslgSKcg==."
-del /Q "lPg7wmAA64wCTygslgSKcg==.png"
-copy /Y "\Users\James Mcguire\Data\Inbox.txt" .
-FlickrStorage encode Inbox.txt password=testtest
-FlickrStorage decode Inbox.txt password=testtest
-fc lPg7wmAA64wCTygslgSKcg==. Inbox.txt
+SET utils=C:\Users\JamesMc\Data\Commands
+
+cd ..\..\SourceCode\FlickrStorage
+msbuild /t:Build /p:Configuration=Release FlickrStorage.csproj
+
+cd bin\release
+COPY /y *.dll %utils%
+COPY /y *.exe %utils%
+COPY /y *.config %utils%
+
+cd ..\..\..\..
+
+del /Q EdVyoUSsrRxnf4Py2heOSA==.
+del /Q lPg7wmAA64wCTygslgSKcg==.png
+copy /Y "%USERPROFILE%\sqlfiles.txt" .
+FlickrStorage encode sqlfiles.txt password=testtest
+FlickrStorage decode sqlfiles.txt password=testtest
+fc EdVyoUSsrRxnf4Py2heOSA==. sqlfiles.txt
