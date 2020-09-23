8a76153b2fa5a7a203b98a731f64fcfa2669faef
JamesMc <jamesjohnmcguire@gmail.com>
2020-09-24 00:28:52 +0900

    Hooked in new library to main application.

--- SourceCode/BackupManager/Program.cs
+++ SourceCode/BackupManager/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BackupManagerLibrary;
+using System;
 
 namespace BackupManager
 {
@@ -6,7 +7,8 @@ namespace BackupManager
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Backup Manager");
+			AccountsManager accounts = new AccountsManager();
 		}
 	}
 }
