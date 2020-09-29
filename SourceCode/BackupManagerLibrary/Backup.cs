fc5c8563a5820acb5deb55ca82a521d0d3bbfbcb
JamesMc <jamesjohnmcguire@gmail.com>
2020-09-29 18:02:36 +0900

    Added CustomInitialization method and code.

--- SourceCode/BackupManagerLibrary/Backup.cs
+++ SourceCode/BackupManagerLibrary/Backup.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
 
 namespace BackupManagerLibrary
@@ -12,14 +15,72 @@ namespace BackupManagerLibrary
 
 			IList<Account> accounts = accountManager.LoadAccounts();
 
-			foreach(Account account in accounts)
+			if (accounts.Count > 0)
 			{
-				bool authenticated = account.Authenticate();
+				CustomInitialization();
 
-				if (authenticated == true)
+				foreach (Account account in accounts)
 				{
+					bool authenticated = account.Authenticate();
 
+					if (authenticated == true)
+					{
+
+					}
 				}
+			}
+		}
+
+		private static void CustomInitialization()
+		{
+			try
+			{
+				Process[] processes = Process.GetProcessesByName("outlook");
+
+				foreach (Process process in processes)
+				{
+					process.CloseMainWindow();
+					process.WaitForExit();
+				}
+
+				// copy PSTs to backup
+				string profilePath = Environment.GetFolderPath(
+					Environment.SpecialFolder.UserProfile);
+				string dataPath = profilePath + @"\Data\ProgramData\Outlook";
+				string backupPath = dataPath + @"\Backups";
+
+				if (System.IO.Directory.Exists(dataPath))
+				{
+					if (!System.IO.Directory.Exists(backupPath))
+					{
+						System.IO.Directory.CreateDirectory(backupPath);
+					}
+
+					string[] files = System.IO.Directory.GetFiles(dataPath);
+
+					foreach (string file in files)
+					{
+						System.IO.FileInfo fileInfo = new FileInfo(file);
+
+						string destination =
+							fileInfo.DirectoryName + @"\Backups\" + fileInfo.Name;
+						System.IO.File.Copy(file, destination);
+					}
+				}
+
+				Process outlook = new Process();
+				outlook.StartInfo.FileName = "outlook.exe";
+				outlook.Start();
+			}
+			catch (Exception exception) when
+				(exception is ArgumentException ||
+				exception is ArgumentNullException ||
+				exception is InvalidOperationException ||
+				exception is PlatformNotSupportedException ||
+				exception is SystemException ||
+				exception is Win32Exception)
+			{
+				// Log
 			}
 		}
 	}
