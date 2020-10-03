0267a1b6dc5acf03518dcde6f1447ab5c54e107f
JamesMc <jamesjohnmcguire@gmail.com>
2020-10-03 22:08:36 +0900

    New classes to match format of json config file.

---
+++ SourceCode/BackupManagerLibrary/Exclude.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackupManagerLibrary
+{
+	public class Exclude
+	{
+		public string Path { get; set; }
+
+		public ExcludeType ExcludeType {get; set; }
+	}
+}
