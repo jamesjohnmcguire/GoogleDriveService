7ea26fb5f7a58c68e079b4ecfdd0710d1e974032
JamesMc <jamesjohnmcguire@gmail.com>
2020-10-03 22:17:56 +0900

    Added new temporary test projects.

---
+++ SourceCode/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		private static readonly ILog Log = LogManager.GetLogger(
+			System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Backup Manager");
+			Log.Info("Log testing.");
+		}
+	}
+}
