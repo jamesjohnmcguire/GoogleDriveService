53a86aba815844fa551ccab15d450f93d1efa559
JamesMc <jamesjohnmcguire@gmail.com>
2020-10-08 16:35:50 +0900

    Added Parent (Google Shared Folder). Added support for ExpandExcludes. Added ExcludesContains to check if given directory is to be excluded.

--- SourceCode/BackupManagerLibrary/Directory.cs
+++ SourceCode/BackupManagerLibrary/Directory.cs
@@ -16,9 +16,43 @@ namespace BackupManagerLibrary
 
 		public string Path { get; set; }
 
+		public string Parent { get; set; }
+
 		public IList<Exclude> Excludes
 		{
 			get { return excludes; }
+		}
+
+		public IList<Exclude> ExpandExcludes()
+		{
+			foreach (Exclude exclude in excludes)
+			{
+				exclude.Path = Environment.ExpandEnvironmentVariables(
+					exclude.Path);
+			}
+
+			return excludes;
+		}
+
+		public bool ExcludesContains(string path)
+		{
+			bool contains = false;
+
+			foreach (Exclude exclude in excludes)
+			{
+				string checkPath = System.IO.Path.GetFullPath(path);
+				string excludeCheckPath =
+					System.IO.Path.GetFullPath(exclude.Path);
+
+				if (checkPath.Equals(
+					excludeCheckPath, StringComparison.OrdinalIgnoreCase))
+				{
+					contains = true;
+					break;
+				}
+			}
+
+			return contains;
 		}
 	}
 }
