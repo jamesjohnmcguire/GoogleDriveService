7f3f6adef3e388ea9d2b94c87cd59d4203f36c90
JamesMc <jamesjohnmcguire@gmail.com>
2020-11-08 19:12:18 +0900

    Additional suppressions of rules I don't agree with.

--- SourceCode/BackupManagerLibrary/GlobalSuppressions.cs
+++ SourceCode/BackupManagerLibrary/GlobalSuppressions.cs
@@ -13,4 +13,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Do not agree with the rule.", Scope = "member", Target = "~M:BackupManagerLibrary.Account.Authenticate~System.Boolean")]
 [assembly: SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "Do not agree with the rule.", Scope = "member", Target = "~M:BackupManagerLibrary.GoogleDrive.CreateFolder(System.String,System.String)~Google.Apis.Drive.v3.Data.File")]
-[assembly: SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "Do not agree with the rule.", Scope = "member", Target = "~M:BackupManagerLibrary.GoogleDrive.Upload(System.String,System.String,System.String)")]
+[assembly: SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "Do not agree with the rule.", Scope = "member", Target = "~M:BackupManagerLibrary.GoogleDrive.Upload(System.String,System.String,System.String,System.Boolean)")]
