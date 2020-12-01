0e626d101ab61bc5ee4d3fc35e3676c1ccd21972
JamesMc <jamesjohnmcguire@gmail.com>
2020-12-01 18:05:08 +0900

    Added AppendTargetFrameworkToOutputPath to false for all projects. Upgraded FlickrStorage.csproj to .NET 5.0.

--- SourceCode/DevelopmentTools/Commands/CopyToCommands.cmd
+++ SourceCode/DevelopmentTools/Commands/CopyToCommands.cmd
@@ -1,6 +1,6 @@
 SET utils=%USERPROFILE%\Data\Commands
 
 CD %~dp0
-cd ..\..\BackUpManager\bin\debug
+cd ..\..\BackUpManager\bin
 
-xcopy /D /E /H /I /R /S /Y netcoreapp3.1 %utils%
+xcopy /D /E /H /I /R /S /Y debug %utils%
