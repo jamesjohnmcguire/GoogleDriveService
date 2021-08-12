5661920b760494e45b7bfa0b424fa9bb939f8d06
JamesMc <jamesjohnmcguire@gmail.com>
2021-08-12 15:48:45 +0900

    Turn off excess output at beginning.

--- SourceCode/DevelopmentTools/GoogleDriveCli/Command.cmd
+++ SourceCode/DevelopmentTools/GoogleDriveCli/Command.cmd
@@ -4,6 +4,7 @@ REM Command list
 REM Command deleteall
 REM Command delete 1IcurHhUiafHbqw8qfw1UEGGLrGGFzDqH
 
+@ECHO OFF
 cd %~dp0
 SET GOOGLE_APPLICATION_CREDENTIALS=%~dp0\GoogleDriveServiceAccount.json
 
