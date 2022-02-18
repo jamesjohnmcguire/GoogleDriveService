1523d0826e9415e738b9dee2313ac83420dbfe91
James John McGuire <jamesjohnmcguire@gmail.com>
2022-02-18 17:08:07 +0900

    Change to IReadOnlyList

--- SourceCode/BackupManagerLibrary/Settings.cs
+++ SourceCode/BackupManagerLibrary/Settings.cs
@@ -12,8 +12,15 @@ using System.Threading.Tasks;
 
 namespace BackupManagerLibrary
 {
-	internal class Settings
+	/// <summary>
+	/// Represents a set of settings.
+	/// </summary>
+	public class Settings
 	{
-		public IList<string> GlobalExcludes { get; set; }
+		/// <summary>
+		/// Gets or sets a list of global excludes.
+		/// </summary>
+		/// <value>A list of global excludes.</value>
+		public IReadOnlyList<string> GlobalExcludes { get; set; }
 	}
 }
