90692eafa3c3f88a6d483e685998024412dd7573
James John McGuire <jamesjohnmcguire@gmail.com>
2022-02-18 15:52:55 +0900

    Add Settings class

---
+++ SourceCode/BackupManagerLibrary/Settings.cs
@@ -0,0 +1,19 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="Settings.cs" company="James John McGuire">
+// Copyright © 2017 - 2022 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupManagerLibrary
+{
+	internal class Settings
+	{
+		public IList<string> GlobalExcludes { get; set; }
+	}
+}
