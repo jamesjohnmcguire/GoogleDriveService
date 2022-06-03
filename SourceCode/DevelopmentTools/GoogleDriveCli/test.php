f91eeb1ee1b4203a22ce45b6f1679f906ee728c9
James John McGuire <jamesjohnmcguire@gmail.com>
2022-06-03 23:22:43 +0900

    Move previous test to its own function

--- SourceCode/DevelopmentTools/GoogleDriveCli/test.php
+++ SourceCode/DevelopmentTools/GoogleDriveCli/test.php
@@ -4,49 +4,48 @@ include_once 'vendor/autoload.php';
 
 defined('CREDENTIALS_FILE') or define('CREDENTIALS_FILE', 'credentials.json');
 
-$client = new Google_Client();
-
-$client->setAccessType('offline');
-$client->setApplicationName('Google Drive API Video Uploader');
-// TODO: Check if this is best scope
-$client->setPrompt('select_account consent');
-$client->setScopes(Google_Service_Drive::DRIVE_FILE);
-
-$client->addScope("https://www.googleapis.com/auth/drive");
-
-$credentialFile = __DIR__ . '/' . CREDENTIALS_FILE;
-
-$client->setAuthConfig($credentialFile);
-
-//$client->setRedirectUri("urn:ietf:wg:oauth:2.0:oob");
-$authorizationUrl = $client->createAuthUrl();
-
-echo 'Open the following link in your browser:' . PHP_EOL;
-echo $authorizationUrl . PHP_EOL;
-echo 'Enter verification code: ';
-
-$authorizationCode = fgets(STDIN);
-$$authorizationCode = trim($authorizationCode);
-echo $authorizationCode . PHP_EOL;
-
-$accessToken = $client->fetchAccessTokenWithAuthCode($authorizationCode);
-echo "ACCESS TOKEN: " . PHP_EOL;
-print_r ($accessToken);
-echo PHP_EOL;
-
-if (array_key_exists('error', $accessToken))
+function TestRawRequestUser()
 {
-	echo "ERROR:" . PHP_EOL;
-	print_r ($accessToken);
+	$client = new Google_Client();
+
+	$client->setAccessType('offline');
+	$client->setApplicationName('Google Drive API Video Uploader');
+	// TODO: Check if this is best scope
+	$client->setPrompt('select_account consent');
+	$client->setScopes(Google_Service_Drive::DRIVE_FILE);
+
+	$client->addScope("https://www.googleapis.com/auth/drive");
+
+	$credentialFile = __DIR__ . '/' . CREDENTIALS_FILE;
+
+	$client->setAuthConfig($credentialFile);
+
+	$authorizationUrl = $client->createAuthUrl();
+
+	echo 'Open the following link in your browser:' . PHP_EOL;
+	echo $authorizationUrl . PHP_EOL;
+	echo 'Enter verification code: ';
+
+	$authorizationCode = fgets(STDIN);
+	$$authorizationCode = trim($authorizationCode);
+	echo $authorizationCode . PHP_EOL;
+
+	$accessToken = $client->fetchAccessTokenWithAuthCode($authorizationCode);
+	echo "ACCESS TOKEN: " . PHP_EOL;
+	print_r($accessToken);
 	echo PHP_EOL;
-}
-else
-{
-	$client->setAccessToken($accessToken);
-	
-	$json =  json_encode($accessToken);
-	$credentialsFile = 'cretentials_new.json';
-	echo "Saving to file: " . $credentialsFile . PHP_EOL;
-	file_put_contents($credentialsFile, $json);
-}
 
+	if (array_key_exists('error', $accessToken))
+	{
+		echo "ERROR:" . PHP_EOL;
+	}
+	else
+	{
+		$client->setAccessToken($accessToken);
+		
+		$json =  json_encode($accessToken);
+		$credentialsFile = 'cretentials_new.json';
+		echo "Saving to file: " . $credentialsFile . PHP_EOL;
+		file_put_contents($credentialsFile, $json);
+	}
+}
