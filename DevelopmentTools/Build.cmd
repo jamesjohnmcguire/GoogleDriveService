f82e3f9eea296f634f366bb16459d47ef95d3c0f
James John McGuire <jamesjohnmcguire@gmail.com>
2023-01-28 21:58:46 +0900

    Optimize Build Script

--- DevelopmentTools/Build.cmd
+++ DevelopmentTools/Build.cmd
@@ -3,14 +3,15 @@ CD ..\SourceCode
 
 IF EXIST Release\NUL rd /s /q Release
 
-dotnet publish --configuration Release --output Release\linux-x64 -p:PublishReadyToRun=true -p:PublishSingleFile=true --runtime linux-x64 --self-contained BackUpManager
-dotnet publish --configuration Release --output Release\osx-x64 -p:PublishReadyToRun=true -p:PublishSingleFile=true --runtime osx-x64 --self-contained BackUpManager
 dotnet publish --configuration Release --output Release\win-x64 -p:PublishReadyToRun=true -p:PublishSingleFile=true --runtime=win-x64 --self-contained BackUpManager
 
 IF "%1"=="release" GOTO release
 GOTO end
 
 :release
+dotnet publish --configuration Release --output Release\linux-x64 -p:PublishReadyToRun=true -p:PublishSingleFile=true --runtime linux-x64 --self-contained BackUpManager
+dotnet publish --configuration Release --output Release\osx-x64 -p:PublishReadyToRun=true -p:PublishSingleFile=true --runtime osx-x64 --self-contained BackUpManager
+
 CD Release\linux-x64
 7z u BackUpManager-linux-x64.zip .
 MOVE BackUpManager-linux-x64.zip ..
