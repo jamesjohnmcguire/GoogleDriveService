e0b0a6b6ed7c33e1f22985daa90a35a891e38351
James John McGuire <jamesjohnmcguire@gmail.com>
2023-03-02 15:50:14 +0900

    Add Interface Based Classes

---
+++ SourceCode/BackupManagerLibrary/GoogleDriveBackUpService.cs
@@ -0,0 +1,23 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="GoogleDriveBackUpService.cs" company="James John McGuire">
+// Copyright © 2017 - 2023 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace BackupManagerLibrary
+{
+	/// <summary>
+	/// Google Drive back up service.
+	/// </summary>
+	public class GoogleDriveBackUpService : IBackUpService
+	{
+		/// <summary>
+		/// Back up method.
+		/// </summary>
+		/// <param name="serviceData">The service data.</param>
+		/// <param name="path">The path to back up.</param>
+		public void BackUp(IBackUpServiceData serviceData, string path)
+		{
+		}
+	}
+}
