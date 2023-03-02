a0d7a888ecddd80f482bab8daf5b5f8f958dafad
James John McGuire <jamesjohnmcguire@gmail.com>
2023-03-02 14:36:13 +0900

    Fill Out Initial Interface Members

--- SourceCode/BackupManagerLibrary/IBackUpService.cs
+++ SourceCode/BackupManagerLibrary/IBackUpService.cs
@@ -1,6 +1,21 @@
-﻿namespace BackupManagerLibrary
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="IBackUpService.cs" company="James John McGuire">
+// Copyright © 2017 - 2023 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace BackupManagerLibrary
 {
+	/// <summary>
+	/// The back up service interface.
+	/// </summary>
 	public interface IBackUpService
 	{
+		/// <summary>
+		/// Back up method.
+		/// </summary>
+		/// <param name="serviceData">The service data.</param>
+		/// <param name="path">The path to back up.</param>
+		public void BackUp(IBackUpServiceData serviceData, string path);
 	}
 }
--- SourceCode/BackupManagerLibrary/IBackUpServiceData.cs
+++ SourceCode/BackupManagerLibrary/IBackUpServiceData.cs
@@ -1,6 +1,20 @@
-﻿namespace BackupManagerLibrary
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="IBackUpServiceData.cs" company="James John McGuire">
+// Copyright © 2017 - 2023 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace BackupManagerLibrary
 {
+	/// <summary>
+	/// The back up service data interface.
+	/// </summary>
 	public interface IBackUpServiceData
 	{
+		/// <summary>
+		/// Gets or sets the Name of service.
+		/// </summary>
+		/// <value>The Name of service.</value>
+		public string Name { get; set; }
 	}
 }
