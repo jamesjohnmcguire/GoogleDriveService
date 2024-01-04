b457a4384361626f58b6dc78b0269717e31d3cd5
James John McGuire <jamesjohnmcguire@gmail.com>
2024-01-04 09:18:50 +0900

    Add Commands Class

---
+++ SourceCode/BackUpManager/Commands.cs
@@ -0,0 +1,18 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="Commands.cs" company="James John McGuire">
+// Copyright © 2017 - 2024 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackUpManager
+{
+	internal class Commands
+	{
+	}
+}
