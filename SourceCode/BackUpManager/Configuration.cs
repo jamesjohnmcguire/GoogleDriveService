2eacc306a3a4e871333d006e60a8d118d3248cd7
James John McGuire <jamesjohnmcguire@gmail.com>
2024-01-04 09:31:26 +0900

    Add Method GetDefaultDataLocation

--- SourceCode/BackUpManager/Configuration.cs
+++ SourceCode/BackUpManager/Configuration.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,20 @@ namespace BackUpManager
 {
 	internal class Configuration
 	{
+		public static string GetDefaultDataLocation()
+		{
+			string defaultDataLocation = null;
+
+			string baseDataDirectory = Environment.GetFolderPath(
+				Environment.SpecialFolder.ApplicationData,
+				Environment.SpecialFolderOption.Create);
+
+			string applicationDataDirectory = @"DigitalZenWorks\BackUpManager";
+
+			defaultDataLocation =
+				Path.Combine(baseDataDirectory, applicationDataDirectory);
+
+			return defaultDataLocation;
+		}
 	}
 }
