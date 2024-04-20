d1f5bae80e73069435b3c8d547eb4d97278d8ec3
James John McGuire <jamesjohnmcguire@gmail.com>
2024-04-20 17:43:01 +0900

    Add Initial Test of RemoveAbandanedFolders

--- SourceCode/BackUp.Tests/UnitTests.cs
+++ SourceCode/BackUp.Tests/UnitTests.cs
@@ -1,3 +1,6 @@
+using DigitalZenWorks.BackUp.Library;
+using NUnit.Framework.Internal;
+
 namespace BackUp.Tests
 {
 	public class Tests
@@ -10,6 +13,29 @@ namespace BackUp.Tests
 		[Test]
 		public void Test1()
 		{
+			Assert.Pass();
+		}
+
+		[Test]
+		public void RemoveAbandanedFoltersTest()
+		{
+			Account accountData = new ();
+
+			using GoogleServiceAccount account =
+				new (accountData);
+			account.IgnoreAbandoned = false;
+
+			string path = Directory.GetCurrentDirectory();
+			string[] subDirectoriesRaw = Directory.GetDirectories(path);
+			IList<string> subDirectories = [.. subDirectoriesRaw];
+
+			account.RemoveAbandonedFolders(
+				path,
+				subDirectories,
+				null,
+				null,
+				null);
+
 			Assert.Pass();
 		}
 	}
