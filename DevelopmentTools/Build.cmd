ade4d14dc2089fe2946753070722c492bf80a0a4
James John McGuire <jamesjohnmcguire@gmail.com>
2026-03-28 14:16:16 +0900

    Add Default Section

--- DevelopmentTools/Build.cmd
+++ DevelopmentTools/Build.cmd
@@ -1,12 +1,23 @@
 CD %~dp0
 CD ..\SourceCode
 
+IF "%1"=="publish" GOTO publish
+IF "%1"=="release" GOTO publish
+
+:default
+dotnet clean /p:Platform="Any CPU"
+dotnet build /p:Platform="Any CPU"
+dotnet test /p:Platform="Any CPU"
+GOTO :EOF
+
+:publish
 IF EXIST Release\NUL rd /s /q Release
 
+dotnet clean BackUpManager
 dotnet publish --configuration Release --output Release\win-x64 -p:PublishReadyToRun=true -p:PublishSingleFile=true -restore --runtime win-x64 --self-contained BackUpManager
 
 IF "%1"=="release" GOTO release
-GOTO end
+GOTO :EOF
 
 :release
 dotnet publish --configuration Release --output Release\linux-x64 -p:PublishReadyToRun=true -p:PublishSingleFile=true --runtime linux-x64 --self-contained BackUpManager
@@ -26,5 +37,3 @@ MOVE BackUpManager-win-x64.zip ..
 
 CD ..
 gh release create v%2 --notes %3 *.zip
-
-:end
